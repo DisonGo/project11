e1c07ba864e0188620c1d298d3d79b151b88472c
DisonGo <jokerj224@gmail.com>
2021-05-24 23:12:54 +0300

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project11';
+  title = '';
+  stickers: any[] = []
+  now:any;
+  text = "";
+  @ViewChild('TextInput') textInput!: ElementRef;
+  idCounter = 0;
+  deleteSticker(id:any){
+    console.log(id);
+    for(let i = 0;i<this.stickers.length;i++){
+      if(this.stickers[i].id == id){
+        this.stickers.splice(i, 1)
+      }
+    }
+  }
+  addNote(title:string,text:string){
+    let buf = title.split("")
+    buf[0] = buf[0].toUpperCase()
+    title = buf.join("")
+    this.now = Date.now();
+    this.stickers.push( { title: title, text: text , id : ++this.idCounter, date: this.now})
+    this.title = ""
+    this.text = ""
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { ElementRef, NgModule, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
